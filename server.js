78e80128ca27eab2de3ff14e104d63ec8fea232b
Wilson Jusuf <wilsonjusuf1998@gmail.com>
2021-01-21 23:41:44 -0800

    feat: add postgres DB connection (#10)
    * feat: add postgres DB connection
    
    * try deploying with db connection
    
    * temporary try
    
    * add pgcrypto extenison
    
    * one more
    
    * final prep before merge
    
    * remove unneeded import in server.js [skip ci]

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+'use strict';
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 
 // have to import using .js extension, a wierd nodejs quirk
 import accountRoutes from './src/accountRoutes.js';
+import { initializePg } from './src/db/dbClient.js';
 
 const app = express();
 if (process.env.NODE_ENV !== 'stonks-dev') {
@@ -12,7 +14,7 @@ if (process.env.NODE_ENV !== 'stonks-dev') {
 
 app.get('/api/health', (req, res) => {
   res.json({
-    "success": "true"
+    "success": true
   });
 });
 
@@ -21,6 +23,24 @@ app.use('/accounts', accountRoutes);
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-  console.log("App listening on port", port);
+console.log("Initializing pg tables if not exists...");
+
+initializePg().then((z) => {
+  app.listen(port, () => {
+    console.log("App listening on port", port);
+  });
+}).catch((e) => {
+  console.error("DB initialization ERROR", e)
+  process.exit(1);
+});
+
+process.on('SIGINT', () => {
+  console.log("SIGINT received - Shutting down server.")
+  process.exit(1);
+
+})
+
+process.on('SIGTERM', () => {
+  console.log("SIGTERM received - Shutting down server.")
+  process.exit(1);
 });
--- src/db/dbClient.js
+++ src/db/dbClient.js
@@ -1,1 +1,27 @@
-// TODO: set up db connections here
+import pg from 'pg';
+const { Client, Pool } = pg;
+
+const connectionString = process.env.PG_CONNSTR || 'postgresql://willyspinner:@localhost:5432/willyspinner';
+
+const pgClient = new Client({ connectionString });
+
+const pgPool = new Pool({ connectionString });
+
+const initializePg = async () => {
+  // first, we connect to the postgres client.
+  await pgClient.connect();
+  await Promise.all([
+    pgClient.query(`
+      CREATE EXTENSION IF NOT EXISTS pgcrypto;
+      CREATE TABLE IF NOT EXISTS USERS (
+        user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        date_created DATE NOT NULL,
+        username TEXT NOT NULL,
+        hashed_password TEXT NOT NULL,
+        email TEXT UNIQUE
+      );
+   `)
+  ]);
+};
+
+export { initializePg, pgClient, pgPool };
